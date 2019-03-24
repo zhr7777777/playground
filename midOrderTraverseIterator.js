21b8f29fca73f7f6dd2d78b68f74853914cd463f
zhr7777777 <simonzhang2016@gmail.com>
2019-03-25 00:05:43 +0800

    Complete midOrderTraverse Iterator

---
+++ midOrderTraverseIterator.js
@@ -0,0 +1,50 @@
+function Node(val) {
+	this.val = val
+	this.left = this.right = null
+}
+
+var root = new Node(1)
+root.left = new Node(2)
+root.right = new Node(3)
+root.left.left = new Node(4)
+root.left.right = new Node(5)
+root.right.left = new Node(6)
+root.right.right = new Node(7)
+
+var createIterator = function (root) {
+	var cur = root
+	var stack = []
+	return {
+		next() {
+			var done = true
+			var value
+			while(cur || stack.length) {
+				done = false
+				if(cur) {
+					stack.push(cur)
+					cur = cur.left
+				} else {
+					let top = stack.pop()
+					cur = top.right
+					value = top.val
+					break
+				}
+			}
+			return {
+				value,
+				done
+			}
+		}
+	}
+}
+
+var i = createIterator(root)
+
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())
+console.log(i.next())

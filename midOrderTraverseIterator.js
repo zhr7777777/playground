a1307847c3fa1d4971c59ba5606fcce1c7b02774
zhr7777777 <simonzhang2016@gmail.com>
2020-01-16 15:14:08 +0800

    Update midOrderTraverseIterator.js

--- midOrderTraverseIterator.js
+++ midOrderTraverseIterator.js
@@ -11,32 +11,47 @@ root.left.right = new Node(5)
 root.right.left = new Node(6)
 root.right.right = new Node(7)
 
-var createIterator = function (root) {
+function *createIterator(root) {
 	var cur = root
 	var stack = []
-	return {
-		next() {
-			var done = true
-			var value
-			while(cur || stack.length) {
-				done = false
-				if(cur) {
-					stack.push(cur)
-					cur = cur.left
-				} else {
-					let top = stack.pop()
-					cur = top.right
-					value = top.val
-					break
-				}
-			}
-			return {
-				value,
-				done
-			}
+	while(cur || stack.length) {
+		if(cur) {
+			stack.push(cur)
+			cur = cur.left
+		} else {
+			let top = stack.pop()
+			cur = top.right
+			yield top.val
 		}
 	}
 }
+
+// var createIterator = function (root) {
+// 	var cur = root
+// 	var stack = []
+// 	return {
+// 		next() {
+// 			var done = true
+// 			var value
+// 			while(cur || stack.length) {
+// 				done = false
+// 				if(cur) {
+// 					stack.push(cur)
+// 					cur = cur.left
+// 				} else {
+// 					let top = stack.pop()
+// 					cur = top.right
+// 					value = top.val
+// 					break
+// 				}
+// 			}
+// 			return {
+// 				value,
+// 				done
+// 			}
+// 		}
+// 	}
+// }
 
 var i = createIterator(root)
 

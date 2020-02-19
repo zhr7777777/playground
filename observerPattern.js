a40d4e1ed97dab6e722780ac0582f8c15759bf4d
zhr7777777 <simonzhang2016@gmail.com>
2020-02-19 14:03:13 +0800

    Update observerPattern.js

--- observerPattern.js
+++ observerPattern.js
@@ -1,16 +1,22 @@
+// publish-subscribe pattern
+
 function Observer(name) {
 	this.name = name
 }
 
 Observer.prototype.observe = function (subject, eventName, cb) {
-	subject.addObserver(this, eventName, cb)
+	subject.observe(this, eventName, cb)
+}
+
+Observer.prototype.notify = function (subject, eventName) {
+	subject.notify(eventName)
 }
 
 function Subject() {
 	this.observerMap = {}
 }
 
-Subject.prototype.addObserver = function (observer, eventName, cb) {
+Subject.prototype.observe = function (observer, eventName, cb) {
 	console.log(observer.name + ' subscribed ' + eventName)
 	if(eventName in this.observerMap) {
 		this.observerMap[eventName].push(cb)
@@ -42,5 +48,40 @@ obs2.observe(sub, 'Chinese new year', () => {
 
 setTimeout(() => {
 	console.log('Chinese new year is coming!!!')
-	sub.notify('Chinese new year')
+	obs1.notify(sub, 'Chinese new year')
 }, 1000)
+
+// observer pattern
+
+function Observer(name) {
+	this.name = name
+	this.list = []
+}
+
+Observer.prototype.observe = function (target, cb) {
+	target.list.push(cb)
+}
+
+Observer.prototype.notify = function(msg) {
+	console.log(msg)
+	this.list.forEach(cb => {
+		cb(msg)
+	})
+}
+
+var obs1 = new Observer('Haoran')
+var obs2 = new Observer('Xiaoyu')
+var obs3 = new Observer('Jingqiu')
+
+obs1.observe(obs3, msg => {
+	console.log(`${obs1.name} got ${msg} from ${obs3.name}`)
+})
+
+obs2.observe(obs3, msg => {
+	console.log(`${obs2.name} got ${msg} from ${obs3.name}`)
+})
+
+obs3.notify(`Chinese new year is coming!!!`)
+
+
+

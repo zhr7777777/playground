f66a704f6fa3d6294f8b980020fb8de2a649fd83
zhr7777777 <simonzhang2016@gmail.com>
2020-05-20 06:31:40 +0800

    Update DFS.js

--- DFS.js
+++ DFS.js
@@ -6,43 +6,57 @@ var data = [{
 		id: 2,
 		children: [{
 			id: 4,
-			children: [{
-				id: 6,
-				children: []
-			}, {
-				id: 7,
-				children: []
-			}]
+			children: [{ id: 6 }, { id: 7 }]
 		}, {
-			id: 5,
-			children: []
+			id: 5
 		}]
 	}, {
-		id: 3,
-		children: []
+		id: 3
 	}]
 }, {
-	id: 1,
-	children: []
+	id: 1
 }]
 
-var DFS = function(data, target) {
+// var DFS = function(data, target) {
+// 	for(let i=0; i<data.length; i++) {
+// 		if(stop) return
+// 		path.push(data[i].id)
+// 		if(data[i].id === target) {
+// 			stop = true
+// 			break
+// 		}
+// 		if(data[i].children && data[i].children.length) {
+// 			DFS(data[i].children, target)
+// 		}
+// 		if(!stop) {
+// 			path.pop()
+// 		}
+// 	}
+// 	return
+// }
+// DFS(data, 2)
+
+// console.log(path)
+
+const DFS = data => {
 	for(let i=0; i<data.length; i++) {
-		if(stop) return
-		path.push(data[i])
-		if(data[i].id === target) {
-			stop = true
-			break
-		}
-		if(data[i].children && data[i].children.length) {
-			DFS(data[i].children, target)
-		}
-		if(!stop) {
-			path.pop()
+		console.log(data[i].id)
+		if(data[i].children) {
+			DFS(data[i].children)
 		}
 	}
-	return
 }
-DFS(data)
 
-console.log(path)
+const BFS = data => {
+	let queue = data
+	while(queue.length) {
+		const top = queue.shift()
+		if(top.children) {
+			queue = queue.concat(top.children)
+		}
+		console.log(top.id)
+	}
+}
+
+DFS(data)
+BFS(data)

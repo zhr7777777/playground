03a86e839a3317b077494a3c39f733714e64cbe4
zhr7777777 <simonzhang2016@gmail.com>
2020-05-24 07:46:16 +0800

    Add sortMethods.js

---
+++ sortMethods.js
@@ -0,0 +1,69 @@
+const arr = [4, 1, 3, 2, -1, -7, 9, 0]
+
+const sort = arr => {
+    return [...arr].sort((a, b) => a - b)
+}
+
+const quickSort = nums => {
+    const sort = (nums, left, right) => {
+        if(left >= right) {
+            return
+        }
+        let base = nums[right]
+        let i = left
+        let j = right
+        while(i < j) {
+            while(i < j && nums[i] < base) {
+                i++
+            }
+            nums[j] = nums[i]
+            while(i < j && nums[j] > base) {
+                j--
+            }
+            nums[i] = nums[j]
+        }
+        nums[j] = base
+        sort(nums, left, i - 1)
+        sort(nums, j + 1, right)
+    }
+    let sortedNums = [...nums]
+    sort(sortedNums, 0, sortedNums.length - 1)
+    return sortedNums
+}
+
+const bubbleSort = arr => {
+    let sortedArr = [...arr]
+    for(let i=0; i<sortedArr.length-1; i++) {
+        for(let j=0; j<sortedArr.length-i-1; j++) {
+            if(sortedArr[j+1] < sortedArr[j]) {
+                let temp = sortedArr[j]
+                sortedArr[j] = sortedArr[j+1]
+                sortedArr[j+1] = temp
+            }
+        }
+    }
+    return sortedArr
+}
+
+const selectSort = arr => {
+    let sortedArr = [...arr]
+    for(let i=0; i<sortedArr.length; i++) {
+        let min = sortedArr[i]
+        let minIndex = i
+        for(let j=i+1; j<sortedArr.length; j++) {
+            if(sortedArr[j] < min) {
+                min = sortedArr[j]
+                minIndex = j
+            }
+        }
+        let temp = sortedArr[minIndex]
+        sortedArr[minIndex] = sortedArr[i]
+        sortedArr[i] = temp
+    }
+    return sortedArr
+}
+
+console.log(sort(arr))
+console.log(quickSort(arr))
+console.log(bubbleSort(arr))
+console.log(selectSort(arr))

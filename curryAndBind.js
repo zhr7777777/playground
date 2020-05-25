7a8efa7facdeebf269a514cbd1e474b5228e2a1f
zhr7777777 <simonzhang2016@gmail.com>
2020-05-25 22:18:24 +0800

    Add lazyMan.js

--- curryAndBind.js
+++ curryAndBind.js
@@ -4,10 +4,11 @@ function bind(fn, context) {
 	}
 }
 
+// 函数柯里化，返回一个已经设置部分参数的函数（原函数有n个参数，生成一个n-m个参数的函数）
 function curry(fn) {
-	var args = Array.from(arguments).slice(1)
-	return function () {
-		return fn.apply(null, args.concat(Array.from(arguments)))
+	var args = Array.from(arguments).slice(1)	// 取出传入curry的参数，先把类数组arguments转化成数组
+	return function () {	// 返回柯里化之后的函数
+		return fn.apply(null, args.concat(Array.from(arguments))) // 之前的参数和传入柯里化之后函数的参数合并
 	}
 }
 
---
+++ lazyMan.js
@@ -0,0 +1,50 @@
+class LazyMan {
+  constructor() {
+    this.queue = []
+    this.begin = true
+  }
+
+  next() {
+    if(this.queue.length) {
+      let first = this.queue.shift()
+      first()
+    }
+  }
+
+  sleepOnly(time) {
+    this.queue.push(() => {
+      console.log('sleep ' + time)
+      setTimeout(() => {
+        this.next()
+      }, time * 1000)
+    })
+    next()
+  }
+
+  sleep(time) {
+    this.queue.push(() => {
+      console.log('sleep ' + time)
+      setTimeout(() => {
+        this.next()
+      }, time * 1000)
+    })
+    return this
+  }
+
+  eat(food) {
+    this.queue.push(() => {
+      console.log('eat ' + food)
+      this.next()
+    })
+    this.next()
+    return this
+  }
+
+}
+
+let man = new LazyMan()
+
+// man.eat('apple').sleep(3).eat('pear').sleep(5)
+// man.sleep(5).eat('apple')
+// man.sleep(5)
+// man.eat('apple')
